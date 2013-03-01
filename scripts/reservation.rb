84eae61e1c631437f77c0961f445ce3804580114
Thomas Howe <howethomas@aol.com>
2013-03-01 18:18:12 -0500

    Example scripts, a few adds

---
+++ scripts/reservation.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require File.expand_path('..') + '/textgen_conversation'
+require 'chronic'
+
+conversation do
+  ask :when_to_cut_hair, 'Welcome to the Hair Port, Cape Cods Best Barber Shop. When are you looking for a hair cut?'
+  reservation_time = Chronic.parse(when_to_cut_hair)
+  record :reservation_time, reservation_time
+  hide :when_to_cut_hair
+  say "Great! We are looking forward to seeing you at #{reservation_time}"
+end
